6e813425403e04b4151b76542d4c56944d820018
dipanshu-create <dipanshuaswal000@gmail.com>
2024-07-12 20:22:49 +0530

    adding first componenet

---
+++ src/components/accordian/data.js
@@ -0,0 +1,23 @@
+const data=[
+    {
+        id:'1',
+        question:'what are accordian components?',
+        answer:'accordion components are user interface elements used for organisation'
+    },
+    {
+        id:'2',
+        question:'what are they used for?',
+        answer:'they are commonly employed in various context, including FAQS'
+    },
+    {
+        id:'3',
+        question:'according as a musical instrument',
+        answer:'the accordian is a musical instrument with a keyboard and bellow',
+    },
+    {
+        id:'4',
+        question:'can i create and accordian component with a different from anaswer',
+        answer:'yes of course, it is very possible to create an accordion component',
+    },
+]
+export default data
