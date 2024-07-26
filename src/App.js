0fe5e7e688125acd590ce19b85e5d60cd3e3ec7b
da185214 <da185214@ncr.com>
2024-07-26 23:28:55 +0530

    adding to accordian

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
-import Accordian from './components/accordian';
+import "./App.css";
+import Accordian from "./components/accordian";
+import React from "react";
 
 function App() {
   return (
     <div className="App">
       {/* Accordian component */}
-      <Accordian>jASDFASDFj</Accordian>  
+      <Accordian>jASDFASDFj</Accordian>
     </div>
   );
 }
