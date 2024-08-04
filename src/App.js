0e2fbc6f1ec779b9b27c7eaff0e8cbcfc2e4588b
da185214 <da185214@ncr.com>
2024-08-04 23:00:41 +0530

    image-slider

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import React from "react";
 import RandomColor from "./components/random-color";
 import StarRating from "./components/star-rating";
 
+import ImageSlider from "./components/image-slider";
+
 function App() {
   return (
     <div className="App">
@@ -11,7 +13,14 @@ function App() {
       {/*<Accordian />*/}
       {/* Random color component */}
       {/*<RandomColor />*/}
-      {<StarRating />}
+      {/*<StarRating />*/}
+      {
+        <ImageSlider
+          url={"https://picsum.photos/v2/list"}
+          page={"1"}
+          limit={"10"}
+        />
+      }
     </div>
   );
 }
--- src/components/image-slider/index.jsx
+++ src/components/image-slider/index.jsx
@@ -1,22 +1,67 @@
+import React from "react";
 import { useEffect, useState } from "react";
+import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
+import "./style.css";
 
-export default function (url, limit) {
+export default function ImageSlider({ url, limit = 5, page = 1 }) {
   const [images, setImages] = useState([]);
   const [currentSlide, setCurrentSlide] = useState(0);
-  const [errorMsg, SetErrorMsg] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
   const [loading, setLoading] = useState(false);
 
-  async function FetchImage(geturl) {
+  async function fetchImage(geturl) {
     try {
-      const response = await fetch(geturl);
+      setLoading(true);
+      const response = await fetch(`${geturl}?page=${page}&limit=${limit}`, {
+        headers: {
+          accept: "application/json",
+          "User-agent": "learning app",
+        },
+      });
       const data = await response.json();
+      if (data) {
+        setImages(data);
+        setLoading(false);
+      }
     } catch (e) {
       setErrorMsg(e.message);
       setLoading(false);
     }
   }
   useEffect(() => {
-    if (url !== "") FetchImage();
+    if (url !== "") fetchImage(url);
   }, [url]);
-  return <div className="container"></div>;
+
+  console.log(images);
+  if (loading) {
+    return <div>Loading data ! Please wait</div>;
+  }
+
+  if (errorMsg !== null) {
+    return <div>Error occured !{errorMsg}</div>;
+  }
+
+  return (
+    <div className="container">
+      <BsArrowLeftCircleFill className="arrow arrow-left" />
+      {images && images.length
+        ? images.map((imageItem) => (
+            <img
+              key={imageItem.id}
+              alt={imageItem.download_url}
+              src={imageItem.download_url}
+              className="current-image"
+            />
+          ))
+        : null}
+      <BsArrowRightCircleFill className="arrow arrow-right" />
+      <span className="circle-indicators">
+        {images && images.length
+          ? images.map((_, index) => (
+              <button key={index} className="current-indicator"></button>
+            ))
+          : null}
+      </span>
+    </div>
+  );
 }
