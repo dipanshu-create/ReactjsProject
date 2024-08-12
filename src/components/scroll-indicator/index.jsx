746bd35576535ac44208514a5e688641cab6509e
da185214 <da185214@ncr.com>
2024-08-12 20:58:44 +0530

    scroll indicator

---
+++ src/components/scroll-indicator/index.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+
+export default function ScrollIndicator({ url }) {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, SetErrorMessage] = useState("");
+  const [scrollPercentage, setScrollPercentage] = useState(0);
+
+  async function fetchData(getUrl) {
+    try {
+      setLoading(true);
+      const response = await fetch(getUrl);
+      const data = await response.json();
+
+      if (data && data.products && data.products.length > 0) {
+        setData(data.products);
+        setLoading(false);
+      }
+    } catch (e) {
+      console.log(e);
+      SetErrorMessage(e.message);
+    }
+    console.log(data);
+  }
+
+  useEffect(() => {
+    fetchData(url);
+  }, [url]);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScrollPercentage);
+    return () => {
+      window.removeEventListener("scroll", () => {});
+    };
+  }, []);
+  return (
+    <div>
+      <h1>custom scroll indicator</h1>
+      <div className="data-container">
+        {data && data.length > 0
+          ? data.map((dataItem) => <p>{dataItem.title}</p>)
+          : null}
+      </div>
+    </div>
+  );
+}
