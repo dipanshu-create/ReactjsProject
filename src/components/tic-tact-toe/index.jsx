66affff2faa7e6e04275f476125e04a931deaa09
Aswal <da185214@ncr.com>
2024-08-30 01:45:42 +0530

    tic tac toe

--- src/components/tic-tact-toe/index.jsx
+++ src/components/tic-tact-toe/index.jsx
@@ -1,3 +1,29 @@
-export default function TicTacToe(){
-    return <div></div>
+import React, { useState } from "react";
+import "./styel.css"
+
+function Square({ value }) {
+  return <button className="square">{value}</button>;
+}
+
+export default function TicTacToe() {
+  const [squares,setSquares]=useState(Array(9).fill(''))
+  return (
+    <div className="tic-tac-toe-container">
+      <div className="row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </div>
+  );
 }
