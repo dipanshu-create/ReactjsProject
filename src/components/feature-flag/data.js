61c7f54f6c2c1980aa31fe51b58e1a0726d6e8e8
Aswal <da185214@ncr.com>
2024-09-19 20:29:47 +0530

    adding index js

--- src/components/feature-flag/data.js
+++ src/components/feature-flag/data.js
@@ -1,4 +1,5 @@
-import { useEffect, useState } from "react";
+
+import Reacts from "react";
 
 const dummyApiResponse = {
   showLightAndDarkMode: false,
@@ -8,25 +9,8 @@ const dummyApiResponse = {
   showTreeView: true,
 };
 
+
 function featureFlagDataServiceCall() {
-  const [loading,setLoading]=useState(false)
-  const [enabledFlags,setEnabledFlags]=useState(null)
-
-
-  async function fetchFeatureFlags(){
-    try{
-      const response=await featureFlagDataServiceCall
-    }catch(error){
-      console.log(error)
-      throw new Error(error)
-    }
-  }
-
-  useEffect(()=>
-    {
-      fetchFeatureFlags():
-    },[])
-
   return new Promise((resolve, reject) => {
     if (dummyApiResponse) setTimeout(resolve(dummyApiResponse), 500);
     else reject("Some error occured! Please try again");
--- src/components/feature-flag/index.jsx
+++ src/components/feature-flag/index.jsx
@@ -1,7 +1,32 @@
-
+import React from "react"
+import LightDarkMode from "../light-dark-mode/inde"
+import TicTacToe from "../tic-tact-toe"
+import RandomColor from "../random-color"
+import Accordian from "../accordian"
+import TreeView from "../tree-view"
 
 
 export default function FeatureFlags(){
+    
+
+const componentsToRender=[
+    {key:'LightDarkMOde',
+    component:<LightDarkMode/>
+    },
+    {key:'showTicTacToe',
+        component:<TicTacToe/>
+    },
+    {key:'showRandomColorGenerator',
+        component:<RandomColor/>
+    },
+    {key:'showAccordian',
+        component:<Accordian/>
+    },
+    {key:'showTreeView',
+        component:<TreeView/>
+}]
+
+
     return (
         <div><h1>feature flags</h1></div>
     )
